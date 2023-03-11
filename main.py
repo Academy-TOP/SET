a1d5dca98a2120a0bd9cbe43f36def5ad5bc72cd
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-11 10:01:46 +0000

    1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ our_set.add("Помидор")
 our_set_2.add("Огурец")
 
 our_set_3 = our_set.union(our_set_2)
-our_set_2.update(our_set)
+our_set.update(our_set_2)
 
-print(our_set_3)
-print(our_set_2)
+print(our_set_2.issubset(our_set_3))
+print(our_set_3.issuperset(our_set_2))
